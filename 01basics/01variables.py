dfbb0594db359924a23e2b294befa765ddf5339e
akshitagarg275 <akshitagarg275@gmail.com>
2024-05-18 10:35:02 +0530

    python class 1

---
+++ 01basics/01variables.py
@@ -0,0 +1,51 @@
+# variables
+
+'''
+The variables are the containers that allows
+us to store the data.
+The values can be changed inside the variables
+
+Python is dynamic programming language
+
+Rules to write a variable name
+- Never give a reserved keyword as a variable name
+- Use alphabets (a-z, A-Z), digits (0-9) and underscore(_)
+- Never begin a variable name with a digit
+- Python is case sensitive language
+
+camel casing
+noOfStudents
+marksObtained
+
+snake casing
+no_of_students
+marks_obtained
+
+'''
+# import keyword
+
+# print(keyword.kwlist)
+
+# age = 15
+
+# name12 = 23
+
+# full_name valid
+# marks$ofstudent not valid
+
+
+full_name = "John doe"
+# print("Line 37: " ,full_name)
+# print("Line 38: full_name")
+
+# TODO: Constants
+'''
+When we do not need to modify the value
+
+'''
+
+PI = 3.14
+print(PI)
+
+PI = "adnsj"
+print(PI)
