b93bd0a428adcd49a280cb128a467e388fff21e7
akshitagarg275 <akshitagarg275@gmail.com>
2024-05-19 09:48:46 +0530

    datatypes

---
+++ 01basics/02datatypes.py
@@ -0,0 +1,95 @@
+# TODO: datatypes
+
+# TODO: Primitive Datatypes
+
+# Numbers
+
+# TODO: int(+ve, -ve, 0)
+# age = 23
+# print("age is: ", age)
+# print("type is: ", type(age))
+
+
+# TODO: float
+# price = 87.23
+# print("price is: ", price)
+# print("type is: ", type(price))
+
+
+# TODO: complex (real num +/- imaginary num)
+# c = 5 + 3j
+# print("complex num is: ", c)
+# print("type is: ", type(c))
+
+# TODO: Boolean (True/False)
+# isLoggedIn = True
+# print("isLoggedIn: ", isLoggedIn)
+# print("type is: ", type(isLoggedIn))
+
+# TODO: strings
+# one line strings: '' , ""
+# s = "we are learning python"
+# print(s)
+# print(type(s))
+
+# s1 = 'Hey, there!' \
+# 'another part'
+# print(s1)
+# print(type(s1))
+
+# msg = ''' Hey, there,
+# How are you doing?
+# All good!
+# '''
+# print(msg)
+# print(type(msg))
+
+
+# TODO: None
+# temp = None
+# print("temp is: ", temp)
+# print("type of temp is: ", type(temp))
+
+
+# TODO: Derived datatypes
+
+# TODO: list []
+# marks = [12,14,12,11,15]
+# print(marks)
+# print(type(marks))
+
+# TODO: tuple ()
+# marks = (12,14,12,11,15)
+# print(marks)
+# print(type(marks))
+
+# TODO: dictionary {key : value}
+stu = {
+    "fname" : "John",
+    "lname" : "Doe",
+    "class" : "python",
+}
+
+# print(stu)
+# print(type(stu))
+
+# TODO: set {}
+s = {1,3,4,5,6,2}
+# print(s)
+# print(type(s))
+
+
+
+# TODO: range
+# range(start, end, step)
+# start is included
+# end is exclusive
+# by default step value is 1 and it is optional
+
+# nums = list(range(1,11))
+# print(nums)
+# print(type(nums))
+
+# mulOf5 = tuple(range(0,51, 5))
+# print(mulOf5)
+# print(type(mulOf5))
